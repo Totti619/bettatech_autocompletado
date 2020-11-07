7e946023923e36d64b712c09a0c9b7a9cf353ea6
Antonio Ortiz <619totti@gmail.com>
2020-11-07 23:23:55 +0100

    sort before apply filter

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import words from './words.ts'
 const prefix = 'pre'
 
 const findWords = (words: Array<string>, prefix: string): Array<string> => {
-    return words.filter((word) => word.startsWith(prefix))
+    return words.sort().filter((word) => word.startsWith(prefix))
 }
 
 const foundWords = findWords(words, prefix)
