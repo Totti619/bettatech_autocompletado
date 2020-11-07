44daf79b8caf0a3e9534ce2ad895648658f45d58
Antonio Ortiz <619totti@gmail.com>
2020-11-07 23:46:48 +0100

    add not linear solution

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
 import words from './words.ts'
 const prefix = 'pre'
 
-const findWords = (words: Array<string>, prefix: string): Array<string> => {
+// initial solution
+const findWordsLinearCost = (words: Array<string>, prefix: string): Array<string> => {
     return words.sort().filter((word) => word.startsWith(prefix))
 }
 
-const foundWords = findWords(words, prefix)
+// alt. solution
+const findWordsNotLinearCost = (words: Array<string>, prefix: string, index: number = 1): Array<string> => {
+    if (index <= prefix.length) {
+        const prefixSubstring = prefix.substring(0, index)
+        const result = words.sort().filter((word) => word.startsWith(prefix))
+        index++
+        return findWordsNotLinearCost(result, prefixSubstring, index)
+    }
+    return words.sort().filter((word) => word.startsWith(prefix))
+}
 
-console.log(foundWords)
+const foundWords = findWordsNotLinearCost(words, prefix)
+
+console.log('[findWordsLinearCost]', findWordsLinearCost(words, prefix))
+console.log('[findWordsNotLinearCost]', findWordsNotLinearCost(words, prefix))
