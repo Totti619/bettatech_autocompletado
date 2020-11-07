2319420fbd081b7ee342639bf846c164b932735b
Antonio Ortiz <619totti@gmail.com>
2020-11-07 22:54:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Solución Autocompletado
+Este script es una solución al problema de *Autocompletado* propuesto por **[BettaTech](https://youtu.be/jIlB1D8e4rk?t=1039)**
+
+### Requisitos:
+- Deno
+
+### Como instalar:
+`git clone `
+
+### Como ejecutarlo:
+`deno run main.ts`
---
+++ main.ts
@@ -0,0 +1,10 @@
+import words from './words.ts'
+const prefix = 'pre'
+
+const findWords = (words: Array<string>, prefix: string): Array<string> => {
+    return words.filter((word) => word.startsWith(prefix))
+}
+
+const foundWords = findWords(words, prefix)
+
+console.log(foundWords)
