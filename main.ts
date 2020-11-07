46f7a1b2655e8bbb0f0de85aaf3bb1203b5ee722
Antonio Ortiz <619totti@gmail.com>
2020-11-08 00:05:22 +0100

    remove sort

--- main.ts
+++ main.ts
@@ -3,18 +3,18 @@ const prefix = 'pre'
 
 // initial solution, 2-3 ms according to console.time
 const findWordsLinearCost = (words: Array<string>, prefix: string): Array<string> => {
-    return words.sort().filter((word) => word.startsWith(prefix))
+    return words.filter((word) => word.startsWith(prefix))
 }
 
 // alt. solution, faster, 1-2 ms according to console.time
 const findWordsNotLinearCost = (words: Array<string>, prefix: string, index: number = 1): Array<string> => {
     if (index <= prefix.length) {
         const altPrefix = prefix.substring(0, index)
-        const result = words.sort().filter((word) => word.startsWith(altPrefix))
+        const result = words.filter((word) => word.startsWith(altPrefix))
         index++
         return findWordsNotLinearCost(result, prefix, index)
     }
-    return words.sort().filter((word) => word.startsWith(prefix))
+    return words.filter((word) => word.startsWith(prefix))
 }
 
 const foundWords = findWordsNotLinearCost(words, prefix)
